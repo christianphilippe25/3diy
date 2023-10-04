1151756bc5d8c3f214d88523e3cebdbdfb948d55
Christian Eichstaedt <57114166+christianphilippe25@users.noreply.github.com>
2023-10-04 14:02:36 -0300

    add

---
+++ src/pages/FindPrinter/FindPrinter.jsx
@@ -0,0 +1,14 @@
+import './css.css'
+
+function FindPrinter(){
+    return(
+        <div id="FindPrinter">
+            <form action="" method="get">
+                <input type="file" name="" id="" />
+                
+            </form>
+        </div>
+    );
+}
+
+export default FindPrinter;
